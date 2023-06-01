8131ce254b2ce3cca0daba3d972cc7c5c764b050
Grigory Korotov <g.korotov@gmail.com>
2023-06-02 02:33:55 +0400

    checkSpam(str) implemented

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  str_lc = '';
+  for (i = 0; i < str.length; i++)
+    str_lc = str_lc + str[i].toLowerCase(); 
+  return (str_lc.includes('1xbet now') || str_lc.includes('free xxxxx'))
 }
