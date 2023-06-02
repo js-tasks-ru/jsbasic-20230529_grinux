aa0d65784c6707abe15c310ef0a44068590f1dbd
Grigory Korotov <g.korotov@gmail.com>
2023-06-02 14:24:07 +0400

    added 'let' for cycle counter

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,7 +2,7 @@ function truncate(str, maxlength) {
   if (str.length <= maxlength)
     return str;
   let str_tr = '';
-  for (i = 0; i < maxlength - 1; i++)
+  for (let i = 0; i < maxlength - 1; i++)
     str_tr = str_tr + str[i];
   str_tr = str_tr + '…';
   return str_tr;
