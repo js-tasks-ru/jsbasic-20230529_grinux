cc68d27433bdc82770772ee077453a341b2abaff
Grigory Korotov <g.korotov@gmail.com>
2023-06-07 22:58:49 +0400

    3-module completed

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  let idx;
+  for (;;) {
+    idx = arr.indexOf('-');
+    if (idx === -1)
+      break;
+    if (idx + 1 < arr.length) //check for trailing '-'
+    {
+      arr[idx + 1] = arr[idx + 1].toUpperCase();
+      arr.splice(idx, 1);
+    }
+  }
+  return arr.join('');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
+// function getMinMax(str) {
+//   let arr = str.split(' ');
+//   let num_arr = arr.filter(item => {
+//     return isFinite(item) && !isNaN(item);
+//   });
+//   num_arr.sort((a, b) => {
+//     return a - b;
+//   });
+//   return {min: +num_arr[0], max: +num_arr[num_arr.length - 1]}
+// }
+
+//вверху выглядит более читаемо, но кажется в js принято делать так, как внизу?
+
 function getMinMax(str) {
-  // ваш код...
+ let num_arr = str.split(' ').filter(item => {
+    return isFinite(item) && !isNaN(item);
+  }).sort((a, b) => {
+    return a - b;
+  });
+  return {min: +num_arr[0], max: +num_arr[num_arr.length - 1]}
 }
