ea95e385349ddf2ea623fff7d4cfed8a3fbd62f8
grinux <g.korotov@gmail.com>
2023-06-19 00:45:03 +0400

    7-module completed

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,93 @@
 import createElement from '../../assets/lib/create-element.js';
 
+class RibbonItem {
+  constructor(item) {
+    if (!item)
+      return;  
+    this._id = item['id'];
+    this._elem = createElement(`
+      <a href="#" class="ribbon__item" data-id="${item['id']}">${item['name']}</a>`);
+  }
+  get elem() {
+    return this._elem;
+  }
+}
+
 export default class RibbonMenu {
   constructor(categories) {
-    this.categories = categories;
+    if (!categories.length)
+      return;
+    this._categories = categories;
+    let ribbon = createElement(`
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+        <nav class="ribbon__inner">
+        </nav>
+      </div>`);
+    this._elem = ribbon;  
+    let ribbon_inner = ribbon.querySelector('.ribbon__inner');
+
+    for(let category of categories)
+      ribbon_inner.appendChild(new RibbonItem(category).elem);
+    this._active_item = ribbon_inner.querySelector('a[data-id=""]');
+    this._active_item.classList.add("ribbon__item_active");//All active by default
+                                    
+    ribbon.appendChild(createElement(`
+      <button class="ribbon__arrow ribbon__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>`));
+
+      let back_btn = ribbon.querySelector(".ribbon__arrow_left");
+      let fwd_btn = ribbon.querySelector(".ribbon__arrow_right");
+      back_btn.classList.remove('ribbon__arrow_visible');
+      fwd_btn.classList.add('ribbon__arrow_visible');
+
+      ribbon.addEventListener('click', event => {
+        let click_src = event.target.closest('button');
+        if (click_src)
+        {
+          if (click_src.classList.contains("ribbon__arrow_left"))
+            ribbon_inner.scrollBy(-350, 0); //back button)
+          else if (click_src.classList.contains("ribbon__arrow_right"))
+            ribbon_inner.scrollBy(350, 0); //forward button)
+        }
+        else
+        {
+          click_src = event.target.closest('a');
+          if (click_src) 
+          {
+            let id = click_src.getAttribute('data-id');
+            if (id) 
+            { 
+              event.preventDefault();
+              console.log('Click source: ', id); 
+              this._active_item.classList.remove('ribbon__item_active');
+              click_src.classList.add('ribbon__item_active');
+              this._active_item = click_src;
+              ribbon.dispatchEvent(new CustomEvent('ribbon-select', {
+                detail: id,
+                bubbles: true
+              }));
+            }
+          }
+        }
+      });
+
+      ribbon_inner.addEventListener('scroll', event => {
+        if (!ribbon_inner.scrollLeft)
+          back_btn.classList.remove('ribbon__arrow_visible');
+        else if (ribbon_inner.scrollWidth - ribbon_inner.scrollLeft - ribbon_inner.clientWidth < 1)
+          fwd_btn.classList.remove('ribbon__arrow_visible');
+        else
+        {
+          back_btn.classList.add('ribbon__arrow_visible');
+          fwd_btn.classList.add('ribbon__arrow_visible');
+        }
+      });
+  }
+  get elem() {
+    return this._elem;
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,64 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    if (steps < 2)
+      return;
+    let slider = createElement(`
+      <div class="slider">
+        <div class="slider__thumb" style="s_fill: 50%;">
+          <span class="slider__value">2</span>
+        </div>
+        <div class="slider__progress" style="width: 50%;"></div>
+        <div class="slider__steps"></div>
+      </div>
+    `);
+    this._elem = slider;
+    this._position = value;
+    let slider_steps = slider.querySelector('.slider__steps');
+    for (let i = 0; i < steps; i++)
+      slider_steps.appendChild(createElement(`<span></span>`));
+    slider_steps.children[this._position].classList.add('slider__step-active');
+    slider.querySelector('.slider__value').innerText = this._position;  
+    let s_fill = 100 / (steps - 1) * this._position;   
+    slider.querySelector('.slider__thumb').style.left = `${s_fill}%`;
+    slider.querySelector('.slider__progress').style.width = `${s_fill}%`; 
+
+    slider.addEventListener('click', event => {
+      let slider_position = slider.getBoundingClientRect();
+      let delta_x = slider_position.width/(steps - 1);
+      let catch_x = slider_position.x + delta_x/2; 
+      //catch_x - граница принадлежности к дискретной позиции
+      //delta_x - шаг увеличения catch_x для проверки принадлежности
+      //к следующей дискретной позиции
+      let new_position = steps - 1;
+      for (let i = 0; i < steps - 1; i++)
+      { 
+        if (event.pageX < catch_x + i * delta_x)
+        {
+          new_position = i;
+          break;
+        }
+      }
+      slider.querySelector('.slider__value').innerText = new_position;
+      slider.querySelector('.slider__steps')
+            .children[this._position]
+            .classList.remove('slider__step-active');  
+      slider.querySelector('.slider__steps')
+            .children[new_position]
+            .classList.add('slider__step-active');
+      s_fill = 100 / (steps - 1) * new_position;      
+      slider.querySelector('.slider__thumb').style.left = `${s_fill}%`;
+      slider.querySelector('.slider__progress').style.width = `${s_fill}%`;      
+      this._position = new_position;
+
+      slider.dispatchEvent(new CustomEvent('slider-change', {
+        detail: this._position,
+        bubbles: true
+      }));
+    });
+  }
+  get elem(){
+    return this._elem;
   }
 }
