ea10cb555edc9d3f0c50273fd96b33900e400ff2
grinux <g.korotov@gmail.com>
2023-06-23 22:39:42 +0400

    8-3 completed

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,49 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product)
+      return;
+    let item = this.cartItems.find(item => {
+      item.product.id === product.id
+    });
+    if (item)
+      item.count++;
+    else {
+      item = {};
+      item.product = product;
+      item.count = 1;
+      this.cartItems.push(item);
+    }
+    this.onProductUpdate(item);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    if (!productId || !amount)
+      return;  
+    let item = this.cartItems.find(item => item.product.id == productId);
+    if (item && amount === 1)
+      item.count++;
+    else if (item && amount === -1)  
+    {
+      item.count--;
+      if (item.count === 0)
+        this.cartItems.splice(this.cartItems.indexOf(item), 1);
+    }
+    else
+      return; //item is undefined or amount is mailformed
+    this.onProductUpdate(item);  
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((amount, item) => amount += item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total += item.count * item.product.price, 0);
   }
 
   onProductUpdate(cartItem) {
